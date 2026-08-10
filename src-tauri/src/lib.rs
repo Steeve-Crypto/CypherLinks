60530193050ef6075765d17938cbc9c7d4b85ec1
Steeve-Crypto <steevecrypto@gmail.com>
2026-08-09 20:02:53 -0400

    Disable unconfigured updater startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1101,7 +1101,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(DownloadState::default())
         .setup(|app| {
             let handle = app.handle().clone();
